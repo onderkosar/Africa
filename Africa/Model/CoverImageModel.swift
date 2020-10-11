80320e20fc0d0cb289e27e211eebe0ee8499d1b4
Önder Koşar <onderkosar@hotmail.com>
2020-10-12 00:15:44 +0300

    json decoding and data fetching functions are created

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Önder Koşar on 11.10.2020.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
