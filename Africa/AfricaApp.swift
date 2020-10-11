59c1e6d7b58fa9d599ea9c2a96697d17bc83e64a
Önder Koşar <onderkosar@hotmail.com>
2020-10-11 18:29:21 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Önder Koşar on 11.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
