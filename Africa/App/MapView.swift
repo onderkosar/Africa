c4dd2f2cb4c958a2acdcc8bfb92fe7353b193cf1
Önder Koşar <onderkosar@hotmail.com>
2020-10-11 23:40:14 +0300

    tab view is created with main screens

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Önder Koşar on 11.10.2020.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
